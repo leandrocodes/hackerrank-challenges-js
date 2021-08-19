35330e2d45d4b30a97290fb621aeae0abe3ae734
Leandro Viana <33972169+leandrocodes@users.noreply.github.com>
2021-08-19 17:17:47 -0400

    Cleaning code

--- warmup/jumpingtheclouds/jumpingOnClouds.js
+++ warmup/jumpingtheclouds/jumpingOnClouds.js
@@ -4,16 +4,11 @@ function jumpingOnClouds(c) {
 	for (let i = 0; i < c.length; i++) {
 		if (c[i] === 0) {
 			if (c[i + 2] === 0 && c[i + 1] === 0) {
-				console.log('jumps 2')
 				jumps++
 				i++
-			} else if (c[i + 2] === 0) {
-				console.log('jumps 2')
+			} else if (c[i + 2] === 0 || c[i + 1] === 0) {
 				jumps++
-			} else if (c[i + 1] === 0) {
-				console.log('jumps 1')
-				jumps++
-			}
+			} 
 		}
 	}
 	console.log(jumps)
